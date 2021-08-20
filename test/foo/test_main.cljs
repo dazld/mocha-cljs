3f9e1acbbe4c7bdf1f2b2f6894dc759f50480869
Dan Peddle <dan@flarework.com>
2021-08-20 06:45:00 +0100

    create jsdom, bind to global

--- test/foo/test_main.cljs
+++ test/foo/test_main.cljs
@@ -1,5 +1,12 @@
 (ns foo.test-main
-  (:require [foo.core-test]))
+  (:require ["jsdom" :as jsdom]
+            [applied-science.js-interop :as j]
+            foo.core-test
+            foo.components.views-test))
+
+(let [dom (jsdom/JSDOM. "<!doctype html><html><body></body></html>")]
+  (j/assoc! js/global :window (.-window dom))
+  (j/assoc! js/global :document (j/get-in dom [:window :document])))
 
 (defn go []
   :does-nothing)
