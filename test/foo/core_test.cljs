9431d6e0ed60f82a9ae9cf8693a7b923f7c25e79
Dan Peddle <dan@flarework.com>
2021-08-20 06:06:31 +0100

    timeout demo

--- test/foo/core_test.cljs
+++ test/foo/core_test.cljs
@@ -1,6 +1,7 @@
 (ns foo.core-test
   (:require [foo.core :as fc]
-            [mocha.support :refer [before-each describe describe-only it it-only xit]]))
+            [mocha.support :refer [time-out
+                                   before-each describe describe-only it it-only xit]]))
 
 (describe "foo/func"
   (let [!count (atom 0)]
@@ -15,23 +16,24 @@
 
 (describe "Promises are handled automatically"
   (it "happy path resolution"
-      (-> (fc/thing+)
-          (.then (fn []
-                   (assert (= 2 2))))))
+    (-> (fc/thing+)
+        (.then (fn []
+                 (assert (= 2 2))))))
   (it "happy path rejection"
+    (let [!caught (atom nil)]
       (-> (fc/thing+)
           (.then (fn []
                    (+ 1 2 3)))
           (.then (fn []
-                   (assert (= 1 2))))))
-  (xit "should catch time outs without any interruption"
-      (js/Promise. (fn [res rej]
-                     ::nope)))
-  (it "should catch rejections"
-      (js/Promise. (fn [res rej]
-                     (rej (js/Error. "nope")))))
-  (it "should catch assertion failures"
-      (js/Promise. (fn [res rej]
-                     (assert (= 1 2) "uhoh")
-                     (res "ok")))))
+                   (assert (= 1 2))))
+          (.catch (fn [err]
+                    (reset! !caught err)))
+          (.then (fn []
+                   (assert (instance? js/Error @!caught)))))))
+  (it "should warn about time outs without any interruption"
+    ;; Try different times here
+    (time-out 40)
+    (js/Promise. (fn [res rej]
+                   (js/setTimeout res 25)))))
+
 
